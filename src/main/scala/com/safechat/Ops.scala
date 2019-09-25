b283e02319b9038abc0f17e1572e38d15423772c
haghard <haghard84@gmail.com>
2019-09-25 18:14:17 +0300

    additional stuff

---
+++ src/main/scala/com/safechat/Ops.scala
@@ -0,0 +1,26 @@
+package com.safechat
+
+import java.net.{InetAddress, NetworkInterface}
+
+import scala.collection.{Map, Seq}
+import scala.jdk.CollectionConverters._
+
+trait Ops {
+  val Opt          = """(\S+)=(\S+)""".r
+  val ethName      = "eth0"
+  val ipExpression = """\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}"""
+
+  def internalAddr: Option[InetAddress] =
+    NetworkInterface.getNetworkInterfaces.asScala.toList
+      .find(_.getName == ethName)
+      .flatMap(x ⇒ x.getInetAddresses.asScala.toList.find(i ⇒ i.getHostAddress.matches(ipExpression)))
+
+  def argsToOpts(args: Seq[String]): Map[String, String] =
+    args.collect { case Opt(key, value) ⇒ key → value }.toMap
+
+  def applySystemProperties(options: Map[String, String]): Unit =
+    for ((key, value) ← options if key startsWith "-D") {
+      println(s"Set $key: $value")
+      System.setProperty(key.substring(2), value)
+    }
+}
--- src/main/scala/com/safechat/Server.scala
+++ src/main/scala/com/safechat/Server.scala
@@ -7,10 +7,14 @@ import com.typesafe.config.{Config, ConfigFactory}
 import akka.actor.typed.scaladsl.adapter._
 import com.safechat.actors.ShardedChats
 import com.safechat.rest.ChatRoomApi
+import scala.collection.Map
 
-object Server {
+import scala.util.Try
 
-  val Dispatcher = "akka.actor.default-dispatcher"
+object Server extends Ops {
+
+  val Dispatcher     = "akka.actor.default-dispatcher"
+  val AkkaSystemName = "echatter"
 
   def guardian(config: Config, hostName: String, httpPort: Int, info: String): Behavior[Nothing] =
     Behaviors
@@ -30,10 +34,46 @@ object Server {
       .narrow
 
   def main(args: Array[String]): Unit = {
-    val host = "192.168.77.10"
-    val port = 9000
+    val opts: Map[String, String] = argsToOpts(args.toList)
+    applySystemProperties(opts)
 
-    val cfg = ConfigFactory.load()
+    val akkaExternalHostName = Option(System.getProperty("akka.remote.artery.canonical.hostname"))
+      .getOrElse(throw new Exception("akka.remote.artery.canonical.hostname is expected"))
+
+    val akkaPort = Try(System.getProperty("akka.remote.artery.canonical.port").toInt)
+      .getOrElse(throw new Exception("akka.remote.artery.canonical.port is expected"))
+
+    val httpPort =
+      Try(System.getProperty("HTTP_PORT").trim.toInt).getOrElse(throw new Exception("HTTP_PORT is expected"))
+
+    val akkaSeeds = Option(System.getProperty("SEEDS")).map { seeds ⇒
+      val seedNodesString = seeds
+        .split(",")
+        .map { node ⇒
+          val ap = node.split(":")
+          s"""akka.cluster.seed-nodes += "akka://$AkkaSystemName@${ap(0)}:${ap(1)}""""
+        }
+        .mkString("\n")
+      (ConfigFactory parseString seedNodesString).resolve()
+    }
+
+    val dbConf = Option(System.getProperty("cassandra.hosts")).map { hs ⇒
+      val contactPoints = hs.split(",").map(h ⇒ s""" "$h" """).mkString(",").dropRight(1)
+      ConfigFactory.parseString(
+        s"""
+             |cassandra-journal.contact-points = [ $contactPoints ]
+             |cassandra-snapshot-store.contact-points = [ $contactPoints ]
+            """.stripMargin
+      )
+    }
+
+    val cfg: Config = {
+      val general = ConfigFactory.empty()
+      val local   = dbConf.fold(general)(c ⇒ general.withFallback(c))
+      akkaSeeds
+        .fold(local)(c ⇒ local.withFallback(c))
+        .withFallback(ConfigFactory.load())
+    }
 
     //check dispatcher name
     cfg.getObject(Dispatcher)
@@ -53,9 +93,9 @@ object Server {
       .append('\n')
       .append("==============================================================================")
       .append('\n')
-      .append(s"Up and running on $host:$port")
+      .append(s"Up and running on $akkaExternalHostName:$akkaPort")
       .toString
 
-    akka.actor.typed.ActorSystem[Nothing](guardian(cfg, host, port, info), "chatter", cfg)
+    akka.actor.typed.ActorSystem[Nothing](guardian(cfg, akkaExternalHostName, httpPort, info), AkkaSystemName, cfg)
   }
 }
