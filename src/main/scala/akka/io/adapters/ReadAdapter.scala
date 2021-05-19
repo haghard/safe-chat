fece81ed107d15d7a62c5024434addc27f04fc99
haghard <haghard84@gmail.com>
2021-05-19 14:17:42 +0300

    merge more

--- src/main/scala/akka/io/adapters/ReadAdapter.scala
+++ src/main/scala/akka/io/adapters/ReadAdapter.scala
@@ -10,6 +10,21 @@ import akka.persistence.journal.ReadEventAdapter
   */
 final class ReadAdapter extends ReadEventAdapter {
 
-  override def fromJournal(event: Any, manifest: String): EventSeq =
+  override def fromJournal(event: Any, manifest: String): EventSeq = {
+    /*
+    Psevdo code
+    case ev: EventV1 =>
+      println("Reading V1 from journal and doing promotion to V3")
+      EventSeq.single(ItemV3(each.id, each.name)))
+
+    case ev: EventV2 =>
+      println("Reading V2 from journal and doing promotion to V3")
+      EventV3(each.id, each.name))
+
+    case ev: EventV3 =>
+      println("V3 event, no promotion needed")
+      EventSeq.single(ev)
+    */
     EventSeq.single(event)
+  }
 }
--- src/main/scala/akka/io/adapters/WriteAdapter.scala
+++ src/main/scala/akka/io/adapters/WriteAdapter.scala
@@ -2,14 +2,17 @@ package akka.io.adapters
 
 import akka.persistence.journal.WriteEventAdapter
 
-/** https://github.com/rockthejvm/akka-persistence/blob/master/src/main/scala/part4_practices/EventAdapters.scala
+/**
+  * https://github.com/rockthejvm/akka-persistence/blob/master/src/main/scala/part4_practices/EventAdapters.scala
+  * https://github.com/calvinlfer/Akka-Persistence-Schema-Evolution-Example/blob/master/src/main/scala/com/experiments/calvin/eventadapters/ShoppingCartEventAdapter.scala
   *
   * WriteEventAdapter - used for backwards compatibility
   *
   * actor -> write event adapter -> serializer -> journal EventAdapter
   */
 final class WriteAdapter extends WriteEventAdapter {
-  override def manifest(event: Any): String = ???
+  override def manifest(event: Any): String = ""
 
+  //always writes the latest event
   override def toJournal(event: Any): Any = event
 }
